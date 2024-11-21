15985726934f613cac24c780cec1d1383dc01fe7
Totm33606 <thomas.chmb@proton.me>
2024-11-21 11:06:54 +0100

    load weights from drive

--- part1/code/settings.py
+++ part1/code/settings.py
@@ -26,4 +26,4 @@ DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 NUM_CLASSES = 10
 CLASS_TO_IDX = {'action': 0, 'animation': 1, 'comedy': 2, 'documentary': 3, 'drama': 4, 'fantasy': 5, 'horror': 6, 'romance': 7, 'science Fiction': 8, 'thriller': 9}
 IDX_TO_CLASS = {v: k for k, v in CLASS_TO_IDX.items()}
-WEIGHTS_PATH = Path("code/resnet18_weights.pth")
+WEIGHTS_PATH = Path("code/resnet18_weights.pt")
